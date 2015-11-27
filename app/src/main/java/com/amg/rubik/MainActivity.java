0edaf762ec06b0c945e2891c38e08eb820271a72
Amarghosh Vadakkoot <amarghosh@gmail.com>
2015-11-27 15:36:20 +0530

    Remove appcompat dependency

--- app/src/main/java/com/amg/rubik/MainActivity.java
+++ app/src/main/java/com/amg/rubik/MainActivity.java
@@ -52,13 +52,15 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*
+        Handle action bar item clicks here. The action bar will
+        automatically handle clicks on the Home/Up button, so long
+        as you specify a parent activity in AndroidManifest.xml.
+        */
+		int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.author) {
             return true;
         }
 
